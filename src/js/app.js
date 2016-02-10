860a4c7f174299ba3c0dd251891153182fbaa362
Tiago Celestino <tcelestino@gmail.com>
2016-02-10 20:42:14 -0200

    doc script and refactor

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,11 @@ var WidgetChaordicOnSite = window.WidgetChaordicOnSite || {};
 WidgetChaordicOnSite = (function () {
   'use strict';
 
+  // default values
   var defaults = {
     template: '#product-list-template',
   };
-
+  
   var privates = {
     $: function (el) {
       return document.querySelector(el);
@@ -27,88 +28,35 @@ WidgetChaordicOnSite = (function () {
     }
   };
 
-  var carousel = {
-    defaults: {
-      widthItem: 170,
-      itemPerPage: 1
-    },
-
-    init: function () {
-      this.carouselWrapper = document.querySelector('.see-more .products-list');
-      this.itensCount = parseInt(this.carouselWrapper.children.length, 10);
-      this.carouselWidth = this.itensCount * carousel.defaults.widthItem + 10;
-      this.carouselWrapper.style.width = this.carouselWidth + 'px';
-      this.btnPrev = document.querySelector('.control-prev');
-      this.btnNext = document.querySelector('.control-next');
-      this.events();
-    },
-    events: function () {
-      this.btnPrev.addEventListener('click', this.prevItem.bind(this));
-      this.btnNext.addEventListener('click', this.nextItem.bind(this));
-    },
-
-    prevItem: function (evt) {
-      var marginLeft = this.directionControl('prev');
-       if (marginLeft > 0) {
-         return;
-       }
-      this.carouselWrapper.style.marginLeft = marginLeft + 'px';
-      evt.preventDefault();
-    },
-
-    nextItem: function (evt) {
-      var marginLeft = this.directionControl('next');
-      var maxWidth = (this.carouselWidth - ((this.itensCount - 4) * carousel.defaults.widthItem)) * -1; //-1020
-      if (marginLeft < maxWidth) {
-        return;
-      }
-      console.log(marginLeft);
-      this.carouselWrapper.style.marginLeft = marginLeft + 'px';
-      evt.preventDefault();
-    },
-
-    getWrapperMarginLeft: function () {
-      var style = this.carouselWrapper.currentStyle ||
-                  window.getComputedStyle(this.carouselWrapper);
-
-
-      return parseInt(style.marginLeft, 10);
-    },
-
-    directionControl: function (direction) {
-      if (direction === 'next') {
-        return (this.getWrapperMarginLeft() - carousel.defaults.itemPerPage * carousel.defaults.widthItem);
-      } else {
-        return (this.getWrapperMarginLeft() + carousel.defaults.itemPerPage * carousel.defaults.widthItem);
-      }
-
-    }
-
-  };
-
   var app = {
+
+    // initialize widget
     init: function (options) {
       this.options = _.extend(defaults, options);
       this.template = privates.template(privates.$(this.options.template).innerHTML);
       this.viewReference(this.getData('reference'));
       this.viewRecommendation(this.getData('recommendation'));
-      carousel.init();
+      Carousel.init();
     },
 
+    // reference view
     viewReference: function (content) {
       var context = this.parseData(content);
       this.setContent('.viewed .products-list', context);
     },
 
+    // recommendation view
     viewRecommendation: function (content) {
       var context = this.parseData(content);
       this.setContent('.see-more .products-list', context);
     },
 
+    // set content with template
     setContent: function (el, context) {
       privates.$(el).innerHTML = this.template({ data: context });
     },
 
+    // parse data JSON
     parseData: function (data) {
       var arr = [];
       if(!_.isArray(data)) {
@@ -122,10 +70,12 @@ WidgetChaordicOnSite = (function () {
       return arr;
     },
 
+    // return data JSON
     getData: function (ref) {
       return this.options.data.data[ref];
     },
 
+    // template to data JSON
     setObjs: function (content) {
       return {
         id: content.businessId,
